d03e37f3630c006d0af161eca4a4e5bfc74dffde
kunalsingh23 <kunsingh@umich.edu>
2017-12-09 20:42:56 -0500

    new files + insta cahce

--- finalproject.py
+++ finalproject.py
@@ -7,7 +7,8 @@ from plotly.offline import plot
 import plotly.graph_objs as go
 import plotly.plotly as py
 from bs4 import BeautifulSoup
-# 													PART 1, FACEBOOK API
+import datetime
+#										PART 1, FACEBOOK API
 
 fb_token = 'EAACEdEose0cBAJ8EbTBHTnQBjWSiAivAFryBxAtpZAlRVM7VvCLgcn4gr7lZAfnUkwlpUuFLVDpZCTUTjUweZCuTm1ncOmbGjxnIY4tqT4MGbHgADnY2BaM500zuMribCgOeI9YfsHK60hZCbNrMMUxjZBxV1XorWFG8dBkbCsoT97a6zk6yiitwlGXX07qrMZD'
 #token expires often, but data is cached for this class so we don't have to keep calling the data
@@ -44,44 +45,73 @@ def request_fb():
 			return ('Problem Caching')
 
 fbdata = request_fb()
+timestamps = []
 
-month_numbers_count = {}
-month_count = {}
+for dic in (fbdata['likes']['data']):
+	timestamps.append(dic['created_time'])
 
-for x in fbdata['likes']['data']:
-	timestamp = (x['created_time'])
-	month = timestamp.split('-')[1]
-	
-	if month in month_numbers_count:
-		month_numbers_count[month] += 1
+timeofday = {}
+timeofday["Midnight-5:59AM"] = 0
+timeofday["6AM-11:59AM"] = 0
+timeofday["Noon-5:59PM"] = 0
+timeofday["6PM-11:59PM"] = 0
+
+dayofweek = {}
+dayofweek['Monday'] = 0
+dayofweek['Tuesday'] = 0
+dayofweek['Wednesday'] = 0
+dayofweek['Thursday'] = 0
+dayofweek['Friday'] = 0
+dayofweek['Saturday'] = 0
+dayofweek['Sunday'] = 0
+
+for timestamp in timestamps:
+	date = timestamp.split('T')[0]
+	time = timestamp.split('T')[1][:-5]
+
+	year = (int(date[:4]))
+	month = date[5:7]
+	if month[0] == '0':
+		month = month[1]
 	else:
-		month_numbers_count[month] = 1
+		month = month
+	month = int(month)
 
-for x in month_numbers_count:
-	if x == '01':
-		month_count['Jan'] = month_numbers_count[x]
-	if x == '02':
-		month_count['Feb'] = month_numbers_count[x]
-	if x == '03':
-		month_count['March'] = month_numbers_count[x]
-	if x == '04':
-		month_count['April'] = month_numbers_count[x]
-	if x == '05':
-		month_count['May'] = month_numbers_count[x]
-	if x == '06':
-		month_count['June'] = month_numbers_count[x]
-	if x == '07':
-		month_count['July'] = month_numbers_count[x]
-	if x == '08':
-		month_count['Aug'] = month_numbers_count[x]
-	if x == '09':
-		month_count['Sept'] = month_numbers_count[x]
-	if x == '10':
-		month_count['Oct'] = month_numbers_count[x]
-	if x == '11':
-		month_count['Nov'] = month_numbers_count[x]
-	if x == '12':
-		month_count['Dec'] = month_numbers_count[x]
+	day = date[8:]
+	if day[0] == '0':
+		day = day[1]
+	else:
+		day = day
+	day = int(day)
+
+	day_of_week = datetime.datetime(year,month,day).weekday()
+
+	if day_of_week == 0:
+		dayofweek['Monday'] += 1
+	if day_of_week == 1:
+		dayofweek['Tuesday'] += 1
+	if day_of_week == 2:
+		dayofweek['Wednesday'] += 1
+	if day_of_week == 3:
+		dayofweek['Thursday'] += 1
+	if day_of_week == 4:
+		dayofweek['Friday'] += 1
+	if day_of_week == 5:
+		dayofweek['Saturday'] += 1
+	if day_of_week == 6:
+		dayofweek['Sunday'] += 1
+
+	hour = (int(time[0:2]))
+
+	if (hour >= 0) and (hour <= 5):
+		timeofday["Midnight-5:59AM"] += 1
+	if (hour >= 6) and (hour <= 11):
+		timeofday["6AM-11:59AM"] += 1
+	if (hour >= 12) and (hour <= 17):
+		timeofday["Noon-5:59PM"] += 1
+	if (hour >= 18):
+		timeofday["6PM-11:59PM"] += 1
+
 
 conn = sqlite3.connect('final_project.sqlite')
 cur = conn.cursor()
@@ -96,17 +126,6 @@ for x in fbdata['likes']['data']:
 
 conn.commit()
 
-months = []
-count_months = []
-
-for x in month_count.keys():
-	months.append(x)
-
-for x in month_count.values():
-	count_months.append(x)
-
-months = sorted(months, key = lambda x: month_count[x], reverse = True)
-count_months = sorted(count_months, reverse = True)
 
 # plotly.offline.plot({
 # 	"data": [Bar(x= months, y= count_months)],
@@ -115,8 +134,7 @@ count_months = sorted(count_months, reverse = True)
 
 
 
-
-#													PART 2, GOOGLE MAPS API
+#										PART 2, GOOGLE MAPS API
 
 
 Maps_Cache = "Maps.json"
@@ -171,7 +189,7 @@ def get_timezone(lst):
 	check = lst[0]
 	check = str(check[0])
 
-	if len(Maps_Diction) == 98:
+	if len(Maps_Diction) == 100:
 		print ('Coordinates Cached!')
 				
 	else:
@@ -260,8 +278,7 @@ for x in stadium_timezones:
 
 conn.commit()
 
-
-#														PART 3, ITUNES API
+#										PART 3, ITUNES API
 
 
 iTunes_Cache = "iTunes_cache.json"
@@ -330,6 +347,78 @@ cur.execute('''CREATE TABLE iTunes_Data (song TEXT, length INTEGER, created_time
 for x in (iTunesData['Drake']['results']):
 	cur.execute("INSERT OR IGNORE INTO iTunes_Data (song, length, created_time) VALUES (?, ?, ?)",
 		(x['trackName'], (x['trackTimeMillis']/1000), (x['releaseDate'])))
+
+conn.commit()
+
+
+
+#										PART 4, INSTAGRAM
+
+access_token = '185479668.5205ea7.604c2c4c5a5e41b1b461963fb63913b0'
+
+Insta_Cache = "Insta_cache.json"
+try:
+    Insta_file = open(Insta_Cache, 'r')
+    Insta_contents = Insta_file.read()
+    Insta_file.close()
+    Insta_Diction = json.loads(Insta_contents)
+except:
+    Insta_Diction = {}
+
+
+def Insta():
+	if (len(Insta_Diction['Instagram']['data'])) == 20:
+		print ('Insta Cached!')
+		return (Insta_Diction)
+
+	else:
+		print ('Requesting Insta')
+		x = requests.get('https://api.instagram.com/v1/users/self/media/recent/?access_token=' + access_token)
+		
+		Instadata = x.json()
+
+		try:
+			Insta_Diction['Instagram'] = Instadata    
+			Instadump = json.dumps(Insta_Diction)
+			fw = open(Insta_Cache, 'w')
+			fw.write(Instadump)
+			return (Instadump)
+			fw.close()
+
+		except:
+			return ('Problem Caching')
+
+
+x = Insta()
+
+instadata = {}
+
+for post in x['Instagram']['data']:
+	instadata[post['id']] = {
+	'Caption_Length' : len(post['caption']['text']),
+	'Likes' : post['likes']['count'],
+	'Comments' : post['comments']['count'],
+	'Tags' : len(post['tags']),
+	'Other_Users' : len(post['users_in_photo'])}
+
+
+captagusers = []
+traffic = []
+
+for x in instadata:
+	CTU = (instadata[x]['Caption_Length']) + (3 * instadata[x]['Tags']) + (3 * instadata[x]['Other_Users'])
+	traff = (instadata[x]['Likes']) + (5 * instadata[x]['Comments'])
+	captagusers.append(CTU)
+	traffic.append(traff)
+
+
+cur.execute('DROP TABLE IF EXISTS Insta_Data')
+
+cur.execute('''CREATE TABLE Insta_Data (ID TEXT, caption_length INTEGER, like_count INTEGER, comment_count INTEGER, tag_count INTEGER, other_users_count INTEGER)''')
+
+for x in instadata:
+	cur.execute("INSERT OR IGNORE INTO Insta_Data (ID, caption_length, like_count, comment_count, tag_count, other_users_count) VALUES (?, ?, ?, ?, ?, ?)",
+		(x, (instadata[x]['Caption_Length']), (instadata[x]['Likes']),(instadata[x]['Comments']),(instadata[x]['Tags']), (instadata[x]['Other_Users'])))
 
 conn.commit()
 
